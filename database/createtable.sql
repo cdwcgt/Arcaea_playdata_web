26e60de1242e91a710896f6492cdb8e680a932cb
chinosk114514 <74499927+chinosk114514@users.noreply.github.com>
2021-04-11 13:09:26 +0800

    创建用户表

---
+++ database/createtable.sql
@@ -0,0 +1,16 @@
+create table '[user_id]'
+(
+songname text not null,
+songdiff text,
+songrating real,
+score int not null,
+getptt real,
+pure int,
+purep int,
+far int,
+lost int,
+time int primary key not null,
+userptt real,
+score_str text,
+time_str text
+);
