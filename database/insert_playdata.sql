89652fa94986a034b28c9d00c831fba7d086a920
chinosk114514 <74499927+chinosk114514@users.noreply.github.com>
2021-04-11 13:20:30 +0800

    更新玩家状态，添加数据

--- database/insert_playdata.sql
+++ database/insert_playdata.sql
@@ -1,5 +1,20 @@
+create table IF NOT EXISTS '[user_id]'
+(
+songname text not null,
+songdiff text,
+songrating real,
+score int not null,
+getptt real,
+pure int,
+purep int,
+far int,
+lost int,
+time int primary key not null,
+userptt real,
+score_str text,
+time_str text
+);
 INSERT INTO userinfo (name,id,ptt) VALUES ('[name]','[id]',[ptt]);
 update userinfo set name='[name]',id='[id]',ptt=[ptt] where id like '[id]';
 INSERT INTO '[user_id]' (songname,songdiff,songrating,score,getptt,pure,purep,far,lost,time,userptt,score_str,time_str)
 VALUES ('[songname]','[songdiff]',[songrating],[score],[getptt],[pure],[purep],[far],[lost],[time],[userptt],'[score_str]','[time_str]');
-                                                            
